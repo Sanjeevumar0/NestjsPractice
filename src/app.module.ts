504041544f149532d4ecd78837294c35bbaabd75
sanjeev <sumar@paktolus.com>
2022-12-13 18:16:37 +0530

    postgres installed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { CartModule } from './cart/cart.module';
+import { typeOrmConfig } from './config/typeorm.config';
 @Module({
-  imports: [CartModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    CartModule],
 })
-export class AppModule {}
+export class AppModule { }
